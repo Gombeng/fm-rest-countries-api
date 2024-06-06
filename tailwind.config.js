d017b642a4718d48084ec3dbeb10796adc3c19b1
ardana <msrgombeng@gmail.com>
2024-06-07 01:01:12 +0700

    Remov comment

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,6 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
-
-    // - Dark Blue (Dark Mode Elements): hsl(209, 23%, 22%)
-    // - Very Dark Blue (Dark Mode Background): hsl(207, 26%, 17%)
-    // - Very Dark Blue (Light Mode Text): hsl(200, 15%, 8%)
-    // - Dark Gray (Light Mode Input): hsl(0, 0%, 52%)
-    // - Very Light Gray (Light Mode Background): hsl(0, 0%, 98%)
-    // - White (Dark Mode Text & Light Mode Elements): hsl(0, 0%, 100%)
     colors: {
       "dark-blue": "hsl(209, 23%, 22%)",
       "very-dark-blue-bg": "hsl(207, 26%, 17%)",
