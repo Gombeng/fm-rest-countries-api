2becf5db4638ff412e572ca67a186150698cc21f
ardana <msrgombeng@gmail.com>
2024-06-07 11:57:05 +0700

    Adjust color

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ export default {
       "dark-blue": "hsl(209, 23%, 22%)",
       "very-dark-blue-bg": "hsl(207, 26%, 17%)",
       "very-dark-blue-text": " hsl(200, 15%, 8%)",
-      "dark-gray": "hsl(0, 0%, 52%)",
+      "dark-gray": "hsl(0, 0%, 75%)",
       "very-light-gray": "hsl(0, 0%, 98%)",
       white: "hsl(0, 0%, 100%)",
     },
