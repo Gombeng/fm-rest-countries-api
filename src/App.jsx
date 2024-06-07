16469afa372b17777cf9f38ee2c1fe2ca0eaec3d
ardana <msrgombeng@gmail.com>
2024-06-07 11:58:42 +0700

    Adjust code based on changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,37 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import "./App.css";
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
+import Header from "./components/Header";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const handleToggle = (setter) => {
+    setter((prev) => !prev);
+  };
+
+  useEffect(() => {
+    const rootElement = document.documentElement;
+    if (darkMode) {
+      rootElement.classList.add("light-mode");
+    } else {
+      rootElement.classList.remove("light-mode");
+    }
+  }, [darkMode]);
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/country/:countryName" element={<Detail />} />
-      </Routes>
-    </Router>
+    <>
+      <Header
+        darkMode={darkMode}
+        onToggleDarkMode={() => handleToggle(setDarkMode)}
+      />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/country/:countryName" element={<Detail />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
